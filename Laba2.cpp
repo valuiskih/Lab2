170db0ca914f03ab75c3e072c61d9e7c09d94954
valuiskih <71443251+valuiskih@users.noreply.github.com>
2020-12-09 22:30:26 +0300

    Add files via upload

---
+++ Laba2.cpp
@@ -0,0 +1,32 @@
+﻿//Ввести строку, заменить интервалы между словами на 2 пробела
+#include <stdio.h>
+int main()
+{
+	char m[1000];
+	printf("Enter your string: \n");
+	fgets(m, sizeof(m), stdin);
+	int i = 0;
+	int len = 0;
+	while (m[i] != '\0')
+	{
+		len = i;
+		if (len > 1000)
+		{
+			printf("\nERROR: array size exceeded");
+			return -1;
+		}
+		else 
+			while (!(((m[i] >= 'A') && (m[i] <= 'Z')) || ((m[i] >= 'a') && (m[i] <= 'z'))))
+			{
+				i++;
+			}
+			while (((m[i] >= 'A') && (m[i] <= 'Z')) || ((m[i] >= 'a') && (m[i] <= 'z')))
+			{
+				printf("%c", m[i]);
+				i++;
+			}
+			printf("  ");
+			i++;
+	}
+	return 0;
+}
